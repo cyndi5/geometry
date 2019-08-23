7c8be4a013ec0a77a470b6ca22118fc25f83ae33
Charles Cavanaugh <charles@Charless-MacBook.local>
2019-08-23 16:35:11 -0500

    made CD logo

--- geometry.py
+++ geometry.py
@@ -1,7 +1,7 @@
 import turtle
 import math
 import random
-
+turtle.setup(512,512)
 wn = turtle.Screen()
 wn.bgcolor('white')
 Cyndi = turtle.Turtle()
@@ -23,8 +23,36 @@ def drawSpecial(t, size, repeat):
         drawCircles(t, size)
         t.right(360 / repeat)
 
+def spinCircle(t, size, repeat):
+    for i in range(repeat):
+        t.color('black')
+        t.circle(size)
+        t.right(360 / repeat)
 
-drawSpecial(Cyndi, 70, 7)
+def crescents(t, size, repeat):
+    t.color('black')
+    t.penup()
+    t.goto(60,-150)
+    move_amount = [-80, -50, 30, 0]
+
+    fill_color = ['black','white smoke','black','white smoke']
+    for i in range(repeat):
+        t.color(fill_color[i])
+        t.fillcolor(fill_color[i])
+        t.begin_fill()
+        t.pendown()
+        t.circle(size, steps=50)
+        t.penup()
+        t.end_fill()
+        if move_amount[i] > 0:
+            t.forward(move_amount[i])
+        else:
+            t.back(abs(move_amount[i]))
+
+
+
+crescents(Cyndi, 150, 4)
+
 # hide the cursor/turtle
 Cyndi.hideturtle()
 # keep holding the screen until closed manually
